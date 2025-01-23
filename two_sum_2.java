c49f87ab55939254fe411cdc8f79d64c4d7dea9d
itskavya <kavenk16@gmail.com>
2025-01-23 14:45:36 -0500

    added two sum 2

---
+++ two_sum_2.java
@@ -0,0 +1,18 @@
+class two_sum_2 {
+    // used binary search
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left+1, right+1};
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
