a5f61f39ed2d3d05b6091ef65d0dac103cfa2382
itskavya <kavenk16@gmail.com>
2025-01-26 23:39:35 -0500

    added common node two linked lists

---
+++ common_node_two_linked_lists.java
@@ -0,0 +1,11 @@
+public class common_node_two_linked_lists {
+    ListNode getintersectionnode(ListNode headA, ListNode headB) {
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = (pA == null) ? headB : pA.next;
+            pB = (pB == null) ? headA : pB.next;
+        }
+        return pA;
+    }
+}
