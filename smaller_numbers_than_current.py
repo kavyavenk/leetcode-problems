c006a1e65ba83276e3d4ff5eb64e68c91a583103
itskavya <kavenk16@gmail.com>
2025-11-13 14:41:16 -0500

    added smaller nos than current - O(nlogn) hashmap approach

---
+++ smaller_numbers_than_current.py
@@ -0,0 +1,17 @@
+# O(n log n) time | O(n) space
+from typing import List
+class smaller_numbers_than_current:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        mapping = {}
+        smaller = []
+        sort = sorted(nums)
+        for i in range(len(sort)):
+            if sort[i] not in mapping:
+                mapping[sort[i]] = i
+        for i in range(len(nums)):
+            smaller.append(mapping[nums[i]])
+        return smaller
+
+        #    nums = [8,1,2,2,3]
+        #  sorted = [1,2,2,3,8]
+        # smaller = [0,1,2,3,4] <- return this
