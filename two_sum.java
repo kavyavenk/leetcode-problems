56f40dc9df3b6fa18cbaf243f7e337c6216bef6d
itskavya <kavenk16@gmail.com>
2025-12-29 10:18:06 -0500

    two sum hash map java implementation

--- two_sum.java
+++ two_sum.java
@@ -1,30 +1,34 @@
+// 1. Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// uses hashmap
+
+import java.util.*;
 class two_sum {
     public int[] twoSum(int[] nums, int target) {
-        int arr[];
-        int a=0;
-        int b=0;
-        int c=0;
-        arr=new int[2];
-        if(2<=nums.length && nums.length<= 10000)
-        {
-            for(int i=0;i<nums.length-1;i++)
-            {
-                for(int j=i+1;j<nums.length;j++)
-                {
-                    if(nums[i]+nums[j]==target)
-                    {
-                        a=i;
-                        b=j;
-                        c=1;
-                        break;
-                    }
-                }
-                if(c==1)
-                    break;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
-        arr[0]=a;
-        arr[1]=b;
-        return arr;
+        return new int[]{};
     }
 }
