d269dfce7546a083dfeb5762743c436927f138bd
itskavya <kavenk16@gmail.com>
2026-01-06 02:37:55 -0500

    merge 2 sorted lists

--- merge_two_sorted_lists.java
+++ merge_two_sorted_lists.java
@@ -8,6 +8,24 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+// The new list should be made up of nodes from list1 and list2
+
+// Example 1:
+// Input: list1 = [1,2,4], list2 = [1,3,5]
+// Output: [1,1,2,3,4,5]
+
+// Example 2:
+// Input: list1 = [], list2 = [1,2]
+// Output: [1,2]
+
+// Example 3:
+// Input: list1 = [], list2 = []
+// Output: []
+
+// O(n + m) time and O(1) space
 class merge_two_sorted_lists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
