f538ee8715af23bfb64a680ac631387601704ebf
itskavya <kavenk16@gmail.com>
2026-01-12 01:39:11 -0500

    contains duplicate - set

---
+++ contains_duplicate.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class contains_duplicate {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            set.add(num);
+        }
+        return !(set.size() == nums.length);
+    }
+}
